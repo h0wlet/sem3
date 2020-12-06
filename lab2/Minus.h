4fc03cf91187a21aaf8bf8192dc4e9de5a43c2a2
h0wlet <71941088+h0wlet@users.noreply.github.com>
2020-12-06 17:01:45 +0700

    Create Minus.h

---
+++ lab2/Minus.h
@@ -0,0 +1,23 @@
+#ifndef LAB2_MINUS_H
+#define LAB2_MINUS_H
+
+#include "IOperation.h"
+#include "NotEnoughElementsInStackException.h"
+#include "NotEnoughArgumentException.h"
+
+class Minus : public IOperation {
+public:
+    void execute(const std::list<std::string> &args, Context &context) const override{
+        if (args.size() > 0) {
+            throw NotEnoughArgumentsException("Command doesn't need arguments!");
+        } else {
+            if (context.stackSize() < 2) {
+                throw NotEnoughElementsInStackException("Not enough elements in the stack!");
+            } else {
+                context.pushValue(context.popValue() - context.popValue());
+            }
+        }
+    }
+};
+
+#endif //LAB2_MINUS_H
