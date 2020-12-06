0213acf6911b9ba0a68d0149ae992441965b2571
h0wlet <71941088+h0wlet@users.noreply.github.com>
2020-12-06 17:07:41 +0700

    Update Plus.h

--- lab2/Plus.h
+++ lab2/Plus.h
@@ -1,4 +1,24 @@
-#include "Plus.h"
-#include "OperationMaker.h"
+#ifndef LAB2_PLUS_H
+#define LAB2_PLUS_H
 
-REGISTER_OPERATION(Plus, "+");
+#include "IOperation.h"
+#include "NotEnoughElementsInStackException.h"
+#include "NotEnoughArgumentException.h"
+
+class Plus : public IOperation {
+public:
+    void execute(const std::list<std::string> &args, Context &context) const override{
+        if (args.size() > 0) {
+            throw NotEnoughArgumentsException("Command doesn't need arguments!");
+        } else {
+            if (context.stackSize() < 2) {
+                throw NotEnoughElementsInStackException("Not enough elements in the stack!");
+            } else {
+                context.pushValue(context.popValue() + context.popValue());
+            }
+        }
+    }
+};
+
+#endif //LAB2_PLUS_H
+
