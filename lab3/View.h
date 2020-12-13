0e30b5d7603480d4f5a70d852aaa91c87aeaed53
h0wlet <71941088+h0wlet@users.noreply.github.com>
2020-12-13 19:57:14 +0700

    Create View.h

---
+++ lab3/View.h
@@ -0,0 +1,12 @@
+#ifndef LAB3_VIEW_H
+#define LAB3_VIEW_H
+
+#include "Model.h"
+
+class View {
+public:
+    static void displayField(Model m);
+    static void chooseGamer(int num);
+};
+
+#endif //LAB3_VIEW_H
