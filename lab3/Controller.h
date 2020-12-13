ee2bc216450bf9e5eafbae8a09e05e30cc330030
h0wlet <71941088+h0wlet@users.noreply.github.com>
2020-12-13 19:54:17 +0700

    Create Controller.h

---
+++ lab3/Controller.h
@@ -0,0 +1,11 @@
+#ifndef LAB3_CONTROLLER_H
+#define LAB3_CONTROLLER_H
+
+#include <utility>
+
+class Controller {
+public:
+    static std::pair<int,int> readLineAndColumn();
+};
+
+#endif //LAB3_CONTROLLER_H
