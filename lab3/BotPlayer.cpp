749a397d809f18f88dac83b1af426a3973cdda81
h0wlet <71941088+h0wlet@users.noreply.github.com>
2020-12-22 17:48:44 +0700

    Update BotPlayer.cpp

--- lab3/BotPlayer.cpp
+++ lab3/BotPlayer.cpp
@@ -1,17 +1,11 @@
 #include "Model.h"
 #include "BotPlayer.h"
 
-std::pair<int,int> BotPlayer::checkPlace(Model model){
+std::pair<int,int> BotPlayer::getMove(Model &model, Sign sign) {
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
-            if (model.field[i][j] == ' ')
+            if (model.getField(i,j) == ' ')
                 return {i,j};
         }
     }
-}
-
-Model BotPlayer::moveBotPlayer(Model model, char sign){
-    std::pair<int,int> indexes = checkPlace(model);
-    model.field[indexes.first][indexes.second] = sign;
-    return model;
 }
