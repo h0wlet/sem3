7847d4ef1a651bad4ec7d9c0909d66acb5af62af
h0wlet <71941088+h0wlet@users.noreply.github.com>
2020-12-22 17:42:52 +0700

    Update Model.h

--- lab3/Model.h
+++ lab3/Model.h
@@ -1,14 +1,47 @@
 #ifndef LAB3_MODEL_H
 #define LAB3_MODEL_H
 
+#include <iostream>
+#include "View.h"
+#include <list>
+#include <utility>
+
+enum class Sign {
+    Cross,
+    Zero
+};
+
 class Model {
     bool checkLine();
     bool checkColumn();
     bool checkDiagonal();
-
-public:
     char field[3][3];
+
     void createField();
+    Sign currentPlayer = Sign::Cross;
+
+    std::list<GameObserver*> observers;
+public:
+    char getField(int line, int column){
+        return field[line][column];
+    };
+
+    void setField(std::pair<int, int> move, Sign s);
+
+    void addGameObserver(GameObserver* observer){
+        observers.push_back(observer);
+    };
+    void removeGameObserver(GameObserver* observer){
+        observers.remove(observer);
+    };
+    void notify(){
+        for(auto observer : observers){
+            observer->update();
+        }
+    };
+
+    Sign getCurrentPlayer();
+    bool makeMove(std::pair<int,int> turn, Model &model);
     bool isEndOfGame();
     bool isFilledField();
 };
