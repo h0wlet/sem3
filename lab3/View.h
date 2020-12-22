cfcb6442745537ac1fd8f7796297b14d7bb3fd63
h0wlet <71941088+h0wlet@users.noreply.github.com>
2020-12-22 17:41:29 +0700

    Update View.h

--- lab3/View.h
+++ lab3/View.h
@@ -1,12 +1,20 @@
 #ifndef LAB3_VIEW_H
 #define LAB3_VIEW_H
 
+#include "GameObserver.h"
 #include "Model.h"
 
-class View {
+using namespace std;
+
+class View : GameObserver {
+    Model &_model;
 public:
-    static void displayField(Model m);
+    view(Model &model) : _model(model){
+        model.addGameObserver(this);
+    };
+    static void displayField(Model &model);
     static void chooseGamer(int num);
+    void update() override;
 };
 
 #endif //LAB3_VIEW_H
