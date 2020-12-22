0ec99d183d39811a29077936ae65d306d3e597a0
h0wlet <71941088+h0wlet@users.noreply.github.com>
2020-12-22 17:42:18 +0700

    Update View.cpp

--- lab3/View.cpp
+++ lab3/View.cpp
@@ -1,14 +1,12 @@
 #include "View.h"
 #include <iostream>
 
-using namespace std;
-
-void View::displayField(Model m){
+void View::displayField(Model &model){
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 2; j++) {
-            std::cout << m.field[i][j] << " | ";
+            std::cout << model.getField(i,j) << " | ";
         }
-        std::cout << m.field[i][2] << std::endl;
+        std::cout << model.getField(i,2) << std::endl;
         if (i != 2) std::cout << "---------" << std::endl;
     }
     std::cout << std::endl;
@@ -21,4 +19,17 @@ void View::chooseGamer(int num) {
     cout << "1.Person" << endl;
     cout << "2.BotPlayer" << endl;
     cout << "Enter only number!" << endl;
+}
+
+void View::update() {
+    if (_model.isEndOfGame()){
+        cout << (_model.getCurrentPlayer() == Sign::Cross) ? "Cross player wins!" : "Zero player wins!";
+        return;
+    }
+    if (_model.isFilledField()){
+        cout << "Draw!";
+        return;
+    }
+    displayField(_model);
+    cout << (_model.getCurrentPlayer() == Sign::Cross) ? "Cross player turn: " : "Zero player turn: ";
 }
