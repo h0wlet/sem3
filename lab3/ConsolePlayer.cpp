953d94be6819638cedce7f2dd1f23fcbfa185b40
h0wlet <71941088+h0wlet@users.noreply.github.com>
2020-12-22 17:47:12 +0700

    Update ConsolePlayer.cpp

--- lab3/ConsolePlayer.cpp
+++ lab3/ConsolePlayer.cpp
@@ -1,7 +1,6 @@
 #include "ConsolePlayer.h"
 #include "Controller.h"
 
-Model ConsolePlayer::moveConsolePlayer(Model model, std::pair<int,int> indexes, char sign){
-    model.field[indexes.first - 1][indexes.second - 1] = sign;
-    return model;
+std::pair<int,int> ConsolePlayer::getMove(Model &model, Sign sign){
+    return Controller::readLineAndColumn();
 }
