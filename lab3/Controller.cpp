dc2de1811f0edb7d88cc114f4898e7bdf8338898
h0wlet <71941088+h0wlet@users.noreply.github.com>
2020-12-22 17:46:21 +0700

    Update Controller.cpp

--- lab3/Controller.cpp
+++ lab3/Controller.cpp
@@ -1,14 +1,11 @@
 #include "Controller.h"
 #include <iostream>
+
 using namespace std;
 
 std::pair<int,int> Controller::readLineAndColumn(){
+    cout << "Enter line and column: " << endl;
     int line, column;
     cin >> line >> column;
-    while (line < 1 || line > 3 || column < 1 || column > 3){
-        cout << "Values entered incorrectly!" << endl;
-        cout << "Enter line and column: " << endl;
-        cin >> line >> column;
-    }
     return {line, column};
 }
